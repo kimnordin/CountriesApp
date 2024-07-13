bb3212bb56715ddfa01e0b220c3e3be721bd15db
Kim <nordinsane@gmail.com>
2024-07-13 16:58:22 +0200

    Update CountryDetailView UI

--- CountriesApp/Views/CountryDetail/CountryDetailView.swift
+++ CountriesApp/Views/CountryDetail/CountryDetailView.swift
@@ -11,29 +11,32 @@ struct CountryDetailView: View {
     let country: Country
     
     var body: some View {
-        VStack {
-            AsyncImage(url: URL(string: country.flag.image)) { result in
-                result.image?
-                    .resizable()
-                    .scaledToFit()
-            }
-            Text("Name")
-                .titleView()
-            Text(country.name.common)
-            if let capitals = country.capitals {
-                let capitalTitle = capitals.count > 1 ? "Capitals" : "Capital"
-                Text(capitalTitle)
-                    .titleView()
-                ForEach(capitals, id: \.self) { capital in
-                    Text(capital)
+        ScrollView {
+            VStack {
+                AsyncImage(url: URL(string: country.flag.image)) { result in
+                    result.image?
+                        .resizable()
+                        .scaledToFit()
                 }
+                Text("Name")
+                    .titleView()
+                Text(country.name.common)
+                if let capitals = country.capitals {
+                    let capitalTitle = capitals.count > 1 ? "Capitals" : "Capital"
+                    Text(capitalTitle)
+                        .titleView()
+                    ForEach(capitals, id: \.self) { capital in
+                        Text(capital)
+                    }
+                }
+                Text("Population")
+                    .titleView()
+                Text("\(country.population)")
+                Spacer()
             }
-            Text("Population")
-                .titleView()
-            Text("\(country.population)")
-            Spacer()
         }
         .navigationTitle(country.name.common)
+        .navigationBarTitleDisplayMode(.large)
     }
 }
 
