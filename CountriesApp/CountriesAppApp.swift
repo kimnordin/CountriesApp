dbff0f9e2f519ac9a959fe9e6850234bdd4b0c12
Kim <nordinsane@gmail.com>
2024-07-13 13:43:43 +0200

    Initial Commit

---
+++ CountriesApp/CountriesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesAppApp.swift
+//  CountriesApp
+//
+//  Created by Kim Nordin on 2024-07-13.
+//
+
+import SwiftUI
+
+@main
+struct CountriesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
